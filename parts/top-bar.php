a32568369065fb50e8ec167c5e56ce4e69fb608d
Kamil Davulcuoglu <kam-dav@hotmail.com>
2014-09-10 17:47:31 +0100

    nav updates

--- parts/top-bar.php
+++ parts/top-bar.php
@@ -1,8 +1,8 @@
 <div class="top-bar-container contain-to-grid show-for-medium-up" role="navigation">
     <nav class="top-bar" data-topbar="">
         <ul class="title-area">
-            <li class="name">
-                <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+            <li class="name show-for-large-up">
+                <h1><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/images/95x67-logo.png" /></a></h1>
             </li>
         </ul>
         <section class="top-bar-section">
