73012f682d7c84ac8b76cbd5f57f11d16b7c6ca0
samirgh <lulzylizard@gmail.com>
2014-09-11 21:07:07 +0300

    Updated

--- frontpage.php
+++ frontpage.php
@@ -63,40 +63,30 @@ get_header(); ?>
         </div>
     </div>
 </div>
-<div class="row white">
-    <div class="large-12 columns">
-        <div class="center">
-
-            <?php /* Start loop */ ?>
-            <?php while (have_posts()) : the_post(); ?>
-                <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
-                    <div class="entry-content">
-                        <?php the_content(); ?>
-                    </div>
-                    <footer>
-                        <?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
-                        <p><?php the_tags(); ?></p>
-                    </footer>
-                </article>
-            <?php endwhile; // End the loop ?>
-
-        </div>
-    </div>
-</div>
 
 <div class="gray-bg">
     <div class="row">
-        <div class="large-12 columns spacing info-box">
-            <h2 class="text-center">TRAIN HARD, TRAIN FUN</h2>
-            <hr class="info-hr">
-            <p>Whether you're a beginner or a seasoned athlete, our sessions are designed to be effective and fun at the same time. Regardless of whether you've ever set foot in a gym before or not, we have developed programs and sessions to suit all types of different customers.﻿</p> 
-            <p>You can check out the different sessions we provide in the <a href="#">Classes</a> section.</p>
-            <p>Remember at Muscle Hustle we provide a group, family orientated environment that is friendly. We believe in having fun when doing exercise!!</p>
+        <div class="large-12 columns">
+            <div class="center">
+
+                <?php /* Start loop */ ?>
+                <?php while (have_posts()) : the_post(); ?>
+                    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+
+                        <div class="entry-content">
+                            <?php the_content(); ?>
+                        </div>
+                        <footer>
+                            <?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'FoundationPress'), 'after' => '</p></nav>' )); ?>
+                            <p><?php the_tags(); ?></p>
+                        </footer>
+                    </article>
+                <?php endwhile; // End the loop ?>
+
+            </div>
         </div>
     </div>
 </div>
-
 
 <div class="row">
     <div class="large-4 columns">
