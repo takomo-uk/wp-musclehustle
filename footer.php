6efc4f3e14afe9c81a93763e65c95e1b38bdc2cb
samirgh <lulzylizard@gmail.com>
2014-09-15 17:56:27 +0300

    Fixed bugs

--- footer.php
+++ footer.php
@@ -1,8 +1,27 @@
 </section>
 
+<footer class="row">
+    <?php do_action('foundationPress_before_footer'); ?>
+    <?php dynamic_sidebar("footer-widgets"); ?>
+    <?php do_action('foundationPress_after_footer'); ?>
+</footer>
+<a class="exit-off-canvas"></a>
+
+    <?php do_action('foundationPress_layout_end'); ?>
+    </div>
+</div>
+<?php wp_footer(); ?>
+<?php do_action('foundationPress_before_closing_body'); ?>
+
 <div class="page-footer">
     <div class="row">
         <div class="large-12 columns">
+
+            <ul>
+                <li>
+
+                </li>
+            </ul>
             Muscle hustle &copy; 2014
         </div>
     </div>
