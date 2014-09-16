ca3e0d323a26bc65f8d481a72002fc4dfd146bca
Kamil Davulcuoglu <kam-dav@hotmail.com>
2014-09-16 15:28:11 +0100

    images, about page, sub pages, animation

---
+++ about.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Template Name: About
+*/
+get_header(); ?>
+
+
+<div class="row">
+
+    <div class="large-2 columns outer-spacing animated bounceInRight">
+        <div clas="left">
+            <ul class="side-nav" data-tab>
+                <li><a href="#panel1">Who We Are</a></li>
+                <li><a href="#panel2">Our Philosophyy</a></li>
+                <li><a href="#panel3">Our Team</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="large-10 columns">
+
+        <div class="tabs-content">
+
+            <div class="content active" id="panel1">
+                <div class="info-box outer-spacing animated animation-delay bounceInRight">
+                    <h2 class="m-title">Who we are</h2>
+                    <br>
+
+                    <p><img class="right img-border" src="<?php echo get_template_directory_uri(); ?>/assets/img/images/muscle-hustle-personal-trainer-fitness-1.png" alt="muscle hustle fitness">The trainers at Muscle Hustle are certified fitness professionals, as well as being genuinely friendly and helpful. We are dedicated to assist you in all aspects of your chosen training programme. Our personal, friendly approach always ensures you'll receive the attention, enjoy sessions, have fun and keep coming back for more.</p>
+                    
+                    <p class="quote"><i>"Fabrice has mastered the art of combining informal chat with firm direction, gentle hard challenge and humour"  
+                    <br>- Greg Suart 1-2-1 session</i></p>
+                </div>
+            </div>
+            <!-- End panel 1 -->
+
+            <div class="content" id="panel2">
+                <div class="info-box outer-spacing animated bounceInRight">
+                    <h2 class="m-title">Mind Over Matter</h2>
+                    <br>
+
+                    <p>We’ve created an environment that will help facilitate you on your journey to achieving your goals. We understand that for you to be at your best in your physical training, everything within your circumference as to motivate and encourage you to bring out the best in you.</p>
+
+                    <p>You can be strong as an individual, but there is also strength in numbers. When you lack strength in certain areas our team will help and guide you, whether it is with your dietary or fitness. We have dedicated ourselves to understanding what you require from a fitness training session, combining our fun and carefully constructed circuits with our motivational skills. How many of us have joined gyms wanting to improve our fitness but later finding after a month or two you no longer have the same drive you began with?  Environment is key and Muscle Hustle shall be the partner you require on your journey to a new healthy lifestyle.</p>
+
+                    <p>Muscle Hustle uses group sessions to build team work using a range of fitness and cardio building circuits. Our exercise techniques are designed to be simple but effective at burning calories and toning your body.</p>
+                </div>
+            </div>
+            <!-- End panel 2 -->
+
+            <div class="content" id="panel3">
+                <div class="info-box outer-spacing animated bounceInRight">
+                    <h2 class="m-title">Our Team</h2>
+                    <br>
+
+                    <p>Muscle Hustle has assembled a team of highly competent and friendly fitness professionals.</p>
+                    
+                    <h4>Fabrice Kabenge <img class="right" src="<?php echo get_template_directory_uri(); ?>/assets/img/images/fabrice-kabenge-muscle-hustle-personal-trainer-fitness-1.jpg" alt"Fabrice Kabenge"></h4>
+                    <h5>Director & Head Trainer</h5>
+
+                    <p>An experienced and dynamic Sports coach and Fitness Instructor with over 7 years experience of Sports coaching gained from coaching roles in sports & fitness, professional services and the Youth & education sector. Utilises knowledge of traditional training principles and Interpersonal skills development to create a broad range of group/individual training sessions/plans.</p>
+                
+                <div class="quote">
+                    <p>- Ex Amateur Boxer for St Pancras Boxing Club situated in Kentish Town
+                    <br>- CYQ Gym Instructor Level 2
+                    <br>- Previously was a Sports Peer Facilitator & Assistant Team Leader for Prince's Trust
+                    <br>- BTEC Level 2 Award in Peer Education
+                    <br>- Community Sports Leadership Award Level 2
+                    <br>- LTA Assistant Tennis Coach Level 1(previously coached at Hackney City Tennis club)
+                    <br>- Football Association Coaching Certificate Level 1(previously coached for Leyton Orient Community Sports Programme)
+                    <br>- Completed NSPCC Child Protection Awareness programme
+                    - Safeguarding and child protection Training</p>
+                </div>
+
+                    <p>He's Coaching as taken him on a journey where he has previously found himself Running Fitness sessions for NEET's, a Sports Fun session for News international staff, running a football coaching session  to young offenders on a football coaching course in Feltham young offenders institute with a positive message of change, Designing and running a sports Day for over a 100 Prince's Trust Senior Management Team Staff,amongst many other things.</p>
+
+                    <p>He Founded Muscle Hustle In 2013 and was competing as an Amateur Boxer.</p>
+                </div>
+            </div>
+            <!-- End panel 3 -->
+              
+        </div>
+        <!-- End tabs-content -->
+
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
