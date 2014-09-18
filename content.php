09a2fed0558cf8409f6df51bbd287f9b1aed8cac
samirgh <lulzylizard@gmail.com>
2014-09-18 21:52:05 +0300

    Latest news page

--- content.php
+++ content.php
@@ -7,16 +7,31 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header>
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-		<?php FoundationPress_entry_meta(); ?>
-	</header>
-	<div class="entry-content">
-		<?php the_content(__('Continue reading...', 'FoundationPress')); ?>
-	</div>
-	<footer>
-		<?php $tag = get_the_tags(); if (!$tag) { } else { ?><p><?php the_tags(); ?></p><?php } ?>
-	</footer>
-	<hr />
-</article>
+<div class="row">
+
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+		<div class="large-1 medium-2 columns">
+			<div class="post-meta">
+				<span class='post-day'><?php the_time('d');?></span>
+  				<span class='post-month'><?php the_time('M'); ?></span>
+			</div>
+		</div>
+
+		<div class="large-11 medium-10 columns space-bot">
+
+				<header class="post-title">
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				</header>
+
+				<div class="post-content">
+					<?php the_content(__('Continue reading...', 'FoundationPress')); ?>
+				</div>
+				<footer class="post-footer">
+					<?php $tag = get_the_tags(); if (!$tag) { } else { ?><p><?php the_tags(); ?></p><?php } ?>
+				</footer>
+
+		</div>
+
+	</article>
+</div>
--- index.php
+++ index.php
@@ -1,34 +1,35 @@
 <?php get_header(); ?>
 <div class="row">
-	<div class="small-12 large-8 columns" role="main">
 
-	<?php if ( have_posts() ) : ?>
+	<div class="large-12 columns" role="main">
 
-		<?php do_action('foundationPress_before_content'); ?>
-
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
-
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
-
-		<?php do_action('foundationPress_before_pagination'); ?>
-
-	<?php endif;?>
+		<div class="latest-news space-top">
 
 
+			<?php if ( have_posts() ) : ?>
 
-	<?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
-		</nav>
-	<?php } ?>
+				<?php do_action('foundationPress_before_content'); ?>
 
-	<?php do_action('foundationPress_after_content'); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'content', get_post_format() ); ?>
+				<?php endwhile; ?>
 
+				<?php else : ?>
+					<?php get_template_part( 'content', 'none' ); ?>
+
+				<?php do_action('foundationPress_before_pagination'); ?>
+
+			<?php endif;?>
+	
+		<?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
+			<nav id="post-nav">
+				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
+				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
+			</nav>
+		<?php } ?>
+
+		<?php do_action('foundationPress_after_content'); ?>
+		</div>
 	</div>
-	<?php get_sidebar(); ?>
 </div>
 <?php get_footer(); ?>
