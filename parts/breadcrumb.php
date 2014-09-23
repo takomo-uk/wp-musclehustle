8802427cbe423a501eed68ee2c058c5b8e20dd2b
samirgh <lulzylizard@gmail.com>
2014-09-23 17:26:15 +0300

    top of topbar

---
+++ parts/breadcrumb.php
@@ -0,0 +1,22 @@
+<div class="breadcrumb">
+
+    <div class="row">
+        <div class="large-12 columns">
+            <div class="header-social">
+                <ul>
+                    <li><a href=""><i class="fi-social-facebook"></i></a></li>
+                    <li><a href=""><a href=""><i class="fi-social-twitter"></i></a></li>
+                    <li><a href=""><i class="fi-social-instagram"></i></a></li>
+                    <a href="#" data-reveal-id="searchModal"><li class="search-icon"><i class="fi-magnifying-glass"></i></li></a>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Search modal -->
+<div id="searchModal" class="reveal-modal" data-reveal>
+  <h3>Search the site</h3>
+  <?php get_search_form(); ?>
+</div>
+<!-- / Search modal -->
