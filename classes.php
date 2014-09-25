310997ee4a2c94cfec5142c5723f495c0a7619ac
Kamil Davulcuoglu <kam-dav@hotmail.com>
2014-09-25 15:58:35 +0100

    begun classes, deleted testimonials, other edits

--- classes.php
+++ classes.php
@@ -7,7 +7,84 @@ get_header(); ?>
 
 <div class="row">
     <div class="large-12 columns outer-spacing text-center">
-    <h2 class="m-title">Our Classes</h2>
+        <h2 class="b-title">Our Classes</h2>
+    </div>
+</div>
+
+<div class="row">
+    <div class="large-12 columns info-box">
+        <h3 class="m-title">Overview</h3>
+        <p>Muscle Hustle offers a wide variety of courses, from classes for beginners to the more advanced. Whether it's The Muscle Hustle program, Back to Fitness program or the Couples program you're sure to find a fun and challenging workout that's appropriate for your level of fitness.﻿</p>
+    </div>
+</div>
+
+<div class="row text-center">
+    <div class="large-4 columns">
+        <div class="class-box class-image-1">
+            <div class="center-align">
+                <div class="class-text">
+                    <h3><b>Muscle Hustle Program</b></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="large-4 columns">
+        <div class="class-box class-image-2" data-dropdown="drop2" aria-controls="drop2" aria-expanded="false">
+            <div class="center-align">
+                <div class="class-text">
+                    <h3><b>Back to Fitness Program</b></h3>
+                </div>
+            </div>
+
+            <div id="drop2" data-dropdown-content class="f-dropdown content" aria-hidden="true" tabindex="-1">
+                <div class="text-left drop-text">
+                    <h5>Course 1: The Muscle Hustle Programme</h5>
+                 
+                    <p>The Muscle Hustle Programme is designed to get you up and motivated. Fab will take you through a variety of different and fun exercises to get you up and exited about coming to the gym!</p>
+                    <h5>Course 1: The Muscle Hustle Programme</h5>
+                 
+                    <p>The Muscle Hustle Programme is designed to get you up and motivated. Fab will take you through a variety of different and fun exercises to get you up and exited about coming to the gym!</p>
+                    <h5>Course 1: The Muscle Hustle Programme</h5>
+                 
+                    <p>The Muscle Hustle Programme is designed to get you up and motivated. Fab will take you through a variety of different and fun exercises to get you up and exited about coming to the gym!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="large-4 columns">
+        <div class="class-box class-image-3">
+            <div class="center-align">
+                <div class="class-text">
+                    <h3><b>Couples <br>Classes</b></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row text-center inner-spacing">
+    <div class="large-4 columns">
+        <div class="class-box class-image-4">
+            <div class="center-align">
+                <div class="class-text">
+                    <h3><b>Singles <br>Classes</b></h3>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="large-4 columns">
+        <div class="class-box class-image-5">
+            <div class="center-align">
+                <div class="class-text">
+                    <h3><b>Company Group Fun Days</b></h3>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
